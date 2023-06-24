b71b0ea5a6b279d16705cc03c5c38b3348fe337d
agrwl-prankit <agrwnlprankit77@gmail.com>
2023-06-24 16:38:54 -0400

    added option menu in MainActivity.java

--- app/src/main/java/com/prankit/androidmenu/MainActivity.java
+++ app/src/main/java/com/prankit/androidmenu/MainActivity.java
@@ -1,8 +1,12 @@
 package com.prankit.androidmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // add option menu to this page
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    // add action on clicking menu items
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.home) Toast.makeText(this, "You are already on Home page", Toast.LENGTH_SHORT).show();
+        if (item.getItemId()==R.id.toast) Toast.makeText(this, "You have clicked Toast option", Toast.LENGTH_SHORT).show();
+        if (item.getItemId()==R.id.logout) Toast.makeText(this, "Currently there is no authentication functionality", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
